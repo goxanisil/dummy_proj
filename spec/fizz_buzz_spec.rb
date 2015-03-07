84671f9cf7d03882943556bfa094340050306c64
Kerry Buckley <kerryjbuckley@gmail.com>
2015-03-07 10:08:43 +0000

    Add useless parens to force a merge conflict

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -2,23 +2,23 @@ require "fizz_buzz"
 
 describe FizzBuzz do
   it "returns 'fizz' for multiples of 3" do
-    expect(subject.call 9).to eq "fizz"
+    expect(subject.call 6).to eq("fizz")
   end
 
   it "returns 'buzz' for multiples of 5" do
-    expect(subject.call 10).to eq "buzz"
+    expect(subject.call 10).to eq("buzz")
   end
 
   it "returns 'fizzbuzz' for multiples of 3 and 5" do
-    expect(subject.call 30).to eq "fizzbuzz"
+    expect(subject.call 30).to eq("fizzbuzz")
   end
 
   it "returns the number (as a string) otherwise" do
-    expect(subject.call 8).to eq "8"
+    expect(subject.call 8).to eq("8")
   end
 
   it "works for the first few numbers" do
     result = (1..15).map {|n| subject.call n }
-    expect(result).to eq %w[1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz]
+    expect(result).to eq(%w[1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz])
   end
 end
