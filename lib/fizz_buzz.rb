2e7b9337d3aee1a726e65552af68dbe13add414b
Kerry Buckley <kerryjbuckley@gmail.com>
2015-03-07 10:00:00 +0000

    Extract multiple_of method

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,10 +1,16 @@
 class FizzBuzz
   def call number
     case
-    when number % 15 == 0 then "fizzbuzz"
-    when number % 3 == 0 then "fizz"
-    when number % 5 == 0 then "buzz"
+    when multiple_of(number, 15) then "fizzbuzz"
+    when multiple_of(number, 3) then "fizz"
+    when multiple_of(number, 5) then "buzz"
     else number.to_s
     end
+  end
+
+  private
+
+  def multiple_of number, factor
+    (number % factor).zero?
   end
 end
