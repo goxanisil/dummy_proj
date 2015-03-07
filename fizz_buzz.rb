3101486affa2439bc45d9d7b1ea55683ba2c0481
Kerry Buckley <kerryjbuckley@gmail.com>
2015-03-07 09:54:11 +0000

    Simple fizzbuzz

---
+++ fizz_buzz.rb
@@ -0,0 +1,10 @@
+class FizzBuzz
+  def call number
+    case
+    when number % 15 == 0 then "fizzbuzz"
+    when number % 3 == 0 then "fizz"
+    when number % 5 == 0 then "buzz"
+    else number.to_s
+    end
+  end
+end
---
+++ fizz_buzz_spec.rb
@@ -0,0 +1,19 @@
+require_relative "fizz_buzz"
+
+describe FizzBuzz do
+  it "returns 'fizz' for multiples of 3" do
+    expect(subject.call 6).to eq "fizz"
+  end
+
+  it "returns 'buzz' for multiples of 5" do
+    expect(subject.call 10).to eq "buzz"
+  end
+
+  it "returns 'fizzbuzz' for multiples of 3 and 5" do
+    expect(subject.call 30).to eq "fizzbuzz"
+  end
+
+  it "returns the number (as a string) otherwise" do
+    expect(subject.call 8).to eq "8"
+  end
+end
